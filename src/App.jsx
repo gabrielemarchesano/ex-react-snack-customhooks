ea7eef45cfb1e1d4155d48907dd90ba460b079fa
gabrielemarchesano <marchesano.gabriele@gmail.com>
2026-04-18 17:13:49 +0200

    snack 2: create useDate() hook to print the current date

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
+import useDate from "./hooks/useDate";
 import useSwitch from "./hooks/useSwitch";
 
 function App() {
 
   const [ isOn, toggle ] = useSwitch();
+  const currentDate = useDate();
 
   return (
     <div>
       <h1>Il valore è: {isOn ? "ON" : "OFF"}</h1>
       <button onClick={toggle}>Cambia stato</button>
+      
+      <h1>Data e ora attuali</h1>
+      <p>{currentDate.toLocaleString()}</p>
     </div>
   )
 }
